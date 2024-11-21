03d9fa6d8eccb2dddc6cce57ef2e83cc5c0329e3
aaharidy <abdoharidy72@gmail.com>
2024-11-21 04:51:26 +0200

    option name and constructor defined from the parent

--- includes/automations/integrations/mailerlite/class-integration.php
+++ includes/automations/integrations/mailerlite/class-integration.php
@@ -47,15 +47,6 @@ class Integration extends Integration_Abstract {
 	public $description = 'MailerLite Integration';
 
 	/**
-	 * Option name
-	 *
-	 * @var string
-	 *
-	 * @since 1.0.0
-	 */
-	public $option_name = 'mailerlite';
-
-	/**
 	 * Class names
 	 *
 	 * @var array
@@ -64,13 +55,6 @@ class Integration extends Integration_Abstract {
 		'remote_data'     => Remote_Data::class,
 		'rest_controller' => REST_Controller::class,
 	);
-
-	/**
-	 * Constructor
-	 */
-	public function __construct() {
-		parent::__construct();
-	}
 
 	/**
 	 * Connect the integration

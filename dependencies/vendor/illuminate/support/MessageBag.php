f9f5a7559aee9d5b03c34fe414c9af2d3f6969d9
aaharidy <abdoharidy72@gmail.com>
2024-12-30 19:57:05 +0200

    php 8.2 issue with return and updated ant package

--- dependencies/vendor/illuminate/support/MessageBag.php
+++ dependencies/vendor/illuminate/support/MessageBag.php
@@ -356,6 +356,7 @@ class MessageBag implements Arrayable, Countable, Jsonable, JsonSerializable, Me
      *
      * @return int
      */
+    #[\ReturnTypeWillChange]
     public function count()
     {
         return count($this->messages, COUNT_RECURSIVE) - count($this->messages);
@@ -376,6 +377,7 @@ class MessageBag implements Arrayable, Countable, Jsonable, JsonSerializable, Me
      *
      * @return array
      */
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return $this->toArray();

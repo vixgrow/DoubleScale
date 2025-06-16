ec3ca9efe67529e8c4748bea096ca18ec10fbdf8
omar <omerx9988@gmail.com>
2025-06-16 17:22:40 +0300

    style: update button variant styles for outline and secondary variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,9 +14,9 @@ const buttonVariants = cva(
 				destructive:
 					'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
 				outline:
-					'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+					'border border-input text-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
 				secondary:
-					'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+					'border border-secondary text-secondary shadow-sm hover:bg-secondary/80 hover:text-primary-foreground',
 				ghost: 'hover:bg-accent hover:text-accent-foreground',
 				link: 'text-primary underline-offset-4 hover:underline',
 			},

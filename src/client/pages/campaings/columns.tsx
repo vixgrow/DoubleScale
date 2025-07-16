477c3a0b036b4eceb6e30f05f42907a254166671
omar <omerx9988@gmail.com>
2025-07-16 15:48:47 +0300

    feat: refactor campaign columns to use dynamic headers and add action dropdown menu

--- src/client/pages/campaings/columns.tsx
+++ src/client/pages/campaings/columns.tsx
@@ -1,10 +1,51 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * External dependencies
+ */
 import { ColumnDef } from '@tanstack/react-table';
+
+/**
+ * Internal dependencies
+ */
 import { Campaign, CampaignStatus } from '../../types';
 import { Checkbox } from '@/components/ui/checkbox';
 import { CAMPAIGN_STATUS_COLORS } from './constants';
 import { Badge } from '@/components/ui/badge';
-// You can use a Zod schema here if you want.
-export const columns: ColumnDef<Campaign>[] = [
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import {
+	CopyIcon,
+	DeleteIcon,
+	FallbackCell,
+	FormattedDateCell,
+	SettingsOutlinedIcon,
+	SortedHeaderCell,
+	TimeAgoCell,
+	ViewOutlinedIcon,
+} from '@/components';
+import { getToLink } from '@quillcrm/navigation';
+
+// Add interface for column props
+interface ColumnProps {
+	onDelete: (id: number) => void;
+	duplicate: (id: number) => void;
+	navigate: (path: string) => void;
+}
+
+export const campaignColumns = ({
+	onDelete,
+	duplicate,
+	navigate,
+}: ColumnProps): ColumnDef<Campaign>[] => [
 	{
 		id: 'select',
 		header: ({ table }) => (
@@ -32,11 +73,16 @@ export const columns: ColumnDef<Campaign>[] = [
 	},
 	{
 		accessorKey: 'name',
-		header: 'Campaign Name',
+		header: ({ column }) =>
+			SortedHeaderCell({
+				column,
+				header: __('Campaign Name', 'quillcrm'),
+			}),
 	},
 	{
 		accessorKey: 'status',
-		header: 'Status',
+		header: ({ column }) =>
+			SortedHeaderCell({ column, header: __('Status', 'quillcrm') }),
 		cell: ({ row }) => {
 			const status = row.getValue('status') as CampaignStatus;
 			const colorClasses =
@@ -44,7 +90,10 @@ export const columns: ColumnDef<Campaign>[] = [
 				'bg-muted text-muted-foreground';
 
 			return (
-				<Badge className={colorClasses} variant="outline">
+				<Badge
+					className={`${colorClasses} rounded-[88px] w-full max-w-24 text-center px-0 justify-center py-1.5`}
+					variant="borderTransparent"
+				>
 					{status.charAt(0).toUpperCase() + status.slice(1)}
 				</Badge>
 			);
@@ -52,26 +101,82 @@ export const columns: ColumnDef<Campaign>[] = [
 	},
 	{
 		accessorKey: 'broadcast',
-		header: 'Broadcast',
+		header: ({ column }) =>
+			SortedHeaderCell({ column, header: __('Broadcast', 'quillcrm') }),
+		cell: ({ row }) => <FallbackCell value={row.getValue('broadcast')} />,
 	},
 	{
 		accessorKey: 'created_at',
-		header: 'Created At',
+		header: ({ column }) =>
+			SortedHeaderCell({ column, header: __('Created At', 'quillcrm') }),
+		cell: ({ row }) => <TimeAgoCell value={row.getValue('created_at')} />,
 	},
 	{
 		accessorKey: 'updated_at',
-		header: 'Updated At',
+		header: ({ column }) =>
+			SortedHeaderCell({ column, header: __('Updated At', 'quillcrm') }),
+		cell: ({ row }) => (
+			<FormattedDateCell value={row.getValue('updated_at')} />
+		),
 	},
 	{
 		accessorKey: 'recipients',
-		header: 'Recipients',
+		header: ({ column }) =>
+			SortedHeaderCell({ column, header: __('Recipients', 'quillcrm') }),
+		cell: ({ row }) => <FallbackCell value={row.getValue('recipients')} />,
 	},
 	{
 		accessorKey: 'open_rate',
-		header: 'Open Rate',
+		header: ({ column }) =>
+			SortedHeaderCell({ column, header: __('Open Rate', 'quillcrm') }),
+		cell: ({ row }) => <FallbackCell value={row.getValue('open_rate')} />,
 	},
 	{
 		accessorKey: 'actions',
-		header: 'Actions',
+		header: () => (
+			<div className="text-center">{__('Actions', 'quillcrm')}</div>
+		),
+		cell: ({ row }) => {
+			const campaign = row.original;
+			return (
+				<div className="text-center">
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button
+								variant="ghost"
+								className="h-8 w-8 p-0 focus-visible:border-none focus-visible:outline-none focus-visible:box-shadow-none focus-visible:ring-0"
+							>
+								<SettingsOutlinedIcon />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end">
+							<DropdownMenuItem
+								onClick={() => {
+									navigate(
+										getToLink(`campaigns/${campaign.id}`)
+									);
+								}}
+							>
+								<ViewOutlinedIcon />
+								{__('Overview', 'quillcrm')}
+							</DropdownMenuItem>
+							<DropdownMenuItem
+								onClick={() => duplicate(campaign.id)}
+							>
+								<CopyIcon />
+								{__('Duplicate', 'quillcrm')}
+							</DropdownMenuItem>
+							<DropdownMenuItem
+								onClick={() => onDelete(campaign.id)}
+								className="text-red-500 hover:text-red-500 focus:text-red-500"
+							>
+								<DeleteIcon />
+								{__('Delete', 'quillcrm')}
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			);
+		},
 	},
 ];
--- src/client/pages/campaings/constants/index.ts
+++ src/client/pages/campaings/constants/index.ts
@@ -1,8 +1,8 @@
 import { CAMPAIGN_STATUS, CampaignStatus } from '../../../types';
 
 export const CAMPAIGN_STATUS_COLORS: Record<CampaignStatus, string> = {
-  [CAMPAIGN_STATUS.DRAFT]: 'bg-gray-100 text-gray-600',
-  [CAMPAIGN_STATUS.INACTIVE]: 'bg-gray-200 text-gray-700',
+  [CAMPAIGN_STATUS.DRAFT]: 'bg-gray-200 text-gray-600',
+  [CAMPAIGN_STATUS.INACTIVE]: 'bg-gray-300 text-gray-700',
   [CAMPAIGN_STATUS.ACTIVE]: 'bg-green-100 text-green-600',
   [CAMPAIGN_STATUS.SCHEDULED]: 'bg-blue-100 text-blue-600',
   [CAMPAIGN_STATUS.PROCESSING]: 'bg-amber-100 text-amber-600',

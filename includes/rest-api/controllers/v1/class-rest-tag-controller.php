c3432a2a154a9f822b7ecad93619bf744f5ad77d
omar <omerx9988@gmail.com>
2025-07-27 12:06:51 +0300

    refactor: enhance REST controllers for lists and tags by improving code readability and consistency in method definitions

--- includes/rest-api/controllers/v1/class-rest-tag-controller.php
+++ includes/rest-api/controllers/v1/class-rest-tag-controller.php
@@ -19,7 +19,8 @@ use QuillCRM\Models\Tag_Model;
 /**
  * Tag Controller class
  */
-class REST_Tag_Controller extends REST_Controller {
+class REST_Tag_Controller extends REST_Controller
+{
 
 	/**
 	 * REST Base
@@ -35,57 +36,58 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @since 1.0.0
 	 */
-	public function register_routes() {
+	public function register_routes()
+	{
 
 		register_rest_route(
 			$this->namespace,
 			'/' . $this->rest_base,
 			array(
 				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_items' ),
-					'permission_callback' => array( $this, 'get_items_permissions_check' ),
-					'args'                => array(
-						'keyword'    => array(
-							'description' => __( 'Keyword to search.', 'quillcrm' ),
-							'type'        => 'string',
-							'format'      => 'text-field',
+					'methods' => WP_REST_Server::READABLE,
+					'callback' => array($this, 'get_items'),
+					'permission_callback' => array($this, 'get_items_permissions_check'),
+					'args' => array(
+						'keyword' => array(
+							'description' => __('Keyword to search.', 'quillcrm'),
+							'type' => 'string',
+							'format' => 'text-field',
 						),
-						'per_page'   => array(
-							'description' => __( 'Number of items to fetch.', 'quillcrm' ),
-							'type'        => 'integer',
+						'per_page' => array(
+							'description' => __('Number of items to fetch.', 'quillcrm'),
+							'type' => 'integer',
 						),
-						'page'       => array(
-							'description' => __( 'Page number.', 'quillcrm' ),
-							'type'        => 'integer',
+						'page' => array(
+							'description' => __('Page number.', 'quillcrm'),
+							'type' => 'integer',
 						),
-						'ids'        => array(
-							'description' => __( 'IDs of tags to fetch.', 'quillcrm' ),
-							'type'        => 'array',
-							'items'       => array(
+						'ids' => array(
+							'description' => __('IDs of tags to fetch.', 'quillcrm'),
+							'type' => 'array',
+							'items' => array(
 								'type' => 'integer',
 							),
 						),
 						'contact_id' => array(
-							'description' => __( 'Contact ID.', 'quillcrm' ),
-							'type'        => 'integer',
+							'description' => __('Contact ID.', 'quillcrm'),
+							'type' => 'integer',
 						),
 					),
 				),
 				array(
-					'methods'             => WP_REST_Server::CREATABLE,
-					'callback'            => array( $this, 'create_item' ),
-					'permission_callback' => array( $this, 'create_item_permissions_check' ),
-					'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::CREATABLE ),
+					'methods' => WP_REST_Server::CREATABLE,
+					'callback' => array($this, 'create_item'),
+					'permission_callback' => array($this, 'create_item_permissions_check'),
+					'args' => $this->get_endpoint_args_for_item_schema(WP_REST_Server::CREATABLE),
 				),
 				array(
-					'methods'             => WP_REST_Server::DELETABLE,
-					'callback'            => array( $this, 'deletes_item' ),
-					'permission_callback' => array( $this, 'deletes_item_permissions_check' ),
-					'args'                => array(
+					'methods' => WP_REST_Server::DELETABLE,
+					'callback' => array($this, 'deletes_item'),
+					'permission_callback' => array($this, 'deletes_item_permissions_check'),
+					'args' => array(
 						'ids' => array(
-							'description' => __( 'Tag IDs.', 'quillcrm' ),
-							'type'        => 'array',
+							'description' => __('Tag IDs.', 'quillcrm'),
+							'type' => 'array',
 						),
 					),
 				),
@@ -97,20 +99,20 @@ class REST_Tag_Controller extends REST_Controller {
 			'/' . $this->rest_base . '/(?P<id>[\d]+)',
 			array(
 				array(
-					'methods'             => WP_REST_Server::READABLE,
-					'callback'            => array( $this, 'get_item' ),
-					'permission_callback' => array( $this, 'get_item_permissions_check' ),
+					'methods' => WP_REST_Server::READABLE,
+					'callback' => array($this, 'get_item'),
+					'permission_callback' => array($this, 'get_item_permissions_check'),
 				),
 				array(
-					'methods'             => WP_REST_Server::EDITABLE,
-					'callback'            => array( $this, 'update_item' ),
-					'permission_callback' => array( $this, 'update_item_permissions_check' ),
-					'args'                => $this->get_endpoint_args_for_item_schema( WP_REST_Server::EDITABLE ),
+					'methods' => WP_REST_Server::EDITABLE,
+					'callback' => array($this, 'update_item'),
+					'permission_callback' => array($this, 'update_item_permissions_check'),
+					'args' => $this->get_endpoint_args_for_item_schema(WP_REST_Server::EDITABLE),
 				),
 				array(
-					'methods'             => WP_REST_Server::DELETABLE,
-					'callback'            => array( $this, 'delete_item' ),
-					'permission_callback' => array( $this, 'delete_item_permissions_check' ),
+					'methods' => WP_REST_Server::DELETABLE,
+					'callback' => array($this, 'delete_item'),
+					'permission_callback' => array($this, 'delete_item_permissions_check'),
 				),
 			)
 		);
@@ -123,55 +125,56 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return array
 	 */
-	public function get_item_schema() {
+	public function get_item_schema()
+	{
 		return array(
-			'$schema'    => 'http://json-schema.org/draft-04/schema#',
-			'title'      => 'tag',
-			'type'       => 'object',
+			'$schema' => 'http://json-schema.org/draft-04/schema#',
+			'title' => 'tag',
+			'type' => 'object',
 			'properties' => array(
-				'id'          => array(
-					'description' => __( 'Unique identifier for the object.', 'quillcrm' ),
-					'type'        => 'integer',
-					'readonly'    => true,
+				'id' => array(
+					'description' => __('Unique identifier for the object.', 'quillcrm'),
+					'type' => 'integer',
+					'readonly' => true,
 				),
-				'name'        => array(
-					'description' => __( 'Name of the tag.', 'quillcrm' ),
-					'type'        => 'string',
-					'required'    => true,
+				'name' => array(
+					'description' => __('Name of the tag.', 'quillcrm'),
+					'type' => 'string',
+					'required' => true,
 					'arg_options' => array(
 						'sanitize_callback' => 'sanitize_text_field',
 					),
 				),
-				'slug'        => array(
-					'description' => __( 'An alphanumeric identifier for the tag.', 'quillcrm' ),
-					'type'        => 'string',
+				'slug' => array(
+					'description' => __('An alphanumeric identifier for the tag.', 'quillcrm'),
+					'type' => 'string',
 					'arg_options' => array(
 						'sanitize_callback' => 'sanitize_title',
 					),
 				),
 				'description' => array(
-					'description' => __( 'Description of the tag.', 'quillcrm' ),
-					'type'        => 'string',
+					'description' => __('Description of the tag.', 'quillcrm'),
+					'type' => 'string',
 					'arg_options' => array(
 						'sanitize_callback' => 'sanitize_text_field',
 					),
 				),
-				'status'      => array(
-					'description' => __( 'Status of the tag.', 'quillcrm' ),
-					'type'        => 'string',
-					'enum'        => array( 'active', 'inactive' ),
+				'status' => array(
+					'description' => __('Status of the tag.', 'quillcrm'),
+					'type' => 'string',
+					'enum' => array('active', 'inactive'),
 				),
-				'created_at'  => array(
-					'type'        => 'string',
+				'created_at' => array(
+					'type' => 'string',
 					'description' => 'Created at',
-					'context'     => array( 'view', 'edit', 'embed' ),
-					'readonly'    => true,
+					'context' => array('view', 'edit', 'embed'),
+					'readonly' => true,
 				),
-				'updated_at'  => array(
-					'type'        => 'string',
+				'updated_at' => array(
+					'type' => 'string',
 					'description' => 'Updated at',
-					'context'     => array( 'view', 'edit', 'embed' ),
-					'readonly'    => true,
+					'context' => array('view', 'edit', 'embed'),
+					'readonly' => true,
 				),
 			),
 		);
@@ -186,41 +189,53 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function get_items( $request ) {
+	public function get_items($request)
+	{
 		try {
-			$per_page   = $request->get_param( 'per_page' ) ? $request->get_param( 'per_page' ) : 10;
-			$page       = $request->get_param( 'page' ) ? $request->get_param( 'page' ) : 1;
-			$keyword    = $request->get_param( 'keyword' ) ?? '';
-			$ids        = $request->get_param( 'ids' ) ?? array();
-			$contact_id = $request->get_param( 'contact_id' ) ?? '';
+			$per_page = $request->get_param('per_page') ? $request->get_param('per_page') : 10;
+			$page = $request->get_param('page') ? $request->get_param('page') : 1;
+			$keyword = $request->get_param('keyword') ?? '';
+			$ids = $request->get_param('ids') ?? array();
+			$contact_id = $request->get_param('contact_id') ?? '';
+			$from = $request->get_param('from') ?? null;
+			$to = $request->get_param('to') ?? null;
 
-			if ( ! empty( $ids ) ) {
-				$tags = Tag_Model::whereIn( 'id', $ids )->orderBy( 'created_at', 'desc' )->paginate( $per_page, array( '*' ), 'page', $page );
+			$query = Tag_Model::query();
+			$total_count = $query->count();
 
-				return new WP_REST_Response( $tags, 200 );
+			if (!empty($ids)) {
+				$tags = $query->whereIn('id', $ids)->orderBy('created_at', 'desc')->paginate($per_page, array('*'), 'page', $page);
+
+				return new WP_REST_Response($tags, 200);
 			}
 
-			$tags = Tag_Model::query();
 
-			if ( '' !== $keyword ) {
-				$tags = $tags->where( 'name', 'LIKE', '%' . $keyword . '%' )
-					->orWhere( 'description', 'LIKE', '%' . $keyword . '%' );
+			if ('' !== $keyword) {
+				$tags = $query->where('name', 'LIKE', '%' . $keyword . '%')
+					->orWhere('description', 'LIKE', '%' . $keyword . '%');
 			}
 
-			if ( '' !== $contact_id ) {
-				$tags = $tags->whereDoesntHave(
+			if ('' !== $contact_id) {
+				$tags = $query->whereDoesntHave(
 					'contacts',
-					function( $q ) use ( $contact_id ) {
-						$q->where( 'contact_id', $contact_id );
+					function ($q) use ($contact_id) {
+						$q->where('contact_id', $contact_id);
 					}
 				);
 			}
 
-			$tags = $tags->orderBy( 'created_at', 'desc' )->paginate( $per_page, array( '*' ), 'page', $page );
+			if ($from) {
+				$query->where('created_at', '>=', $from);
+			}
+			if ($to) {
+				$query->where('created_at', '<=', $to);
+			}
 
-			return new WP_REST_Response( $tags, 200 );
-		} catch ( \Exception $e ) {
-			return new WP_Error( 'rest_tag_cannot_read', $e->getMessage(), array( 'status' => 500 ) );
+			$tags = $query->orderBy('created_at', 'desc')->paginate($per_page, array('*'), 'page', $page);
+
+			return new WP_REST_Response($tags->toArray() + array('total_count' => $total_count), 200);
+		} catch (\Exception $e) {
+			return new WP_Error('rest_tag_cannot_read', $e->getMessage(), array('status' => 500));
 		}
 	}
 
@@ -233,8 +248,9 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return bool $response Permission check result.
 	 */
-	public function get_items_permissions_check( $request ) {
-		return current_user_can( 'manage_options' );
+	public function get_items_permissions_check($request)
+	{
+		return current_user_can('manage_options');
 	}
 
 	/**
@@ -246,14 +262,15 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function create_item( $request ) {
+	public function create_item($request)
+	{
 		try {
-			$tag_data = $this->prepare_tag( $request );
-			$tag      = Tag_Model::create( $tag_data );
+			$tag_data = $this->prepare_tag($request);
+			$tag = Tag_Model::create($tag_data);
 
-			return new WP_REST_Response( $tag, 201 );
-		} catch ( \Exception $e ) {
-			return new WP_Error( 'rest_tag_cannot_create', $e->getMessage(), array( 'status' => 500 ) );
+			return new WP_REST_Response($tag, 201);
+		} catch (\Exception $e) {
+			return new WP_Error('rest_tag_cannot_create', $e->getMessage(), array('status' => 500));
 		}
 	}
 
@@ -266,8 +283,9 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return bool $response Permission check result.
 	 */
-	public function create_item_permissions_check( $request ) {
-		return current_user_can( 'manage_options' );
+	public function create_item_permissions_check($request)
+	{
+		return current_user_can('manage_options');
 	}
 
 	/**
@@ -279,17 +297,18 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function get_item( $request ) {
+	public function get_item($request)
+	{
 		try {
-			$tag = Tag_Model::find( $request->get_param( 'id' ) );
+			$tag = Tag_Model::find($request->get_param('id'));
 
-			if ( ! $tag ) {
-				return new WP_Error( 'rest_tag_cannot_read', __( 'Tag not found.', 'quillcrm' ), array( 'status' => 404 ) );
+			if (!$tag) {
+				return new WP_Error('rest_tag_cannot_read', __('Tag not found.', 'quillcrm'), array('status' => 404));
 			}
 
-			return new WP_REST_Response( $tag, 200 );
-		} catch ( \Exception $e ) {
-			return new WP_Error( 'rest_tag_cannot_read', $e->getMessage(), array( 'status' => 500 ) );
+			return new WP_REST_Response($tag, 200);
+		} catch (\Exception $e) {
+			return new WP_Error('rest_tag_cannot_read', $e->getMessage(), array('status' => 500));
 		}
 	}
 
@@ -302,8 +321,9 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return bool $response Permission check result.
 	 */
-	public function get_item_permissions_check( $request ) {
-		return current_user_can( 'manage_options' );
+	public function get_item_permissions_check($request)
+	{
+		return current_user_can('manage_options');
 	}
 
 	/**
@@ -315,21 +335,22 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function update_item( $request ) {
+	public function update_item($request)
+	{
 		try {
-			$tag_id = $request->get_param( 'id' );
-			$tag    = Tag_Model::find( $tag_id );
+			$tag_id = $request->get_param('id');
+			$tag = Tag_Model::find($tag_id);
 
-			if ( ! $tag ) {
-				return new WP_Error( 'rest_tag_cannot_update', __( 'Tag not found.', 'quillcrm' ), array( 'status' => 404 ) );
+			if (!$tag) {
+				return new WP_Error('rest_tag_cannot_update', __('Tag not found.', 'quillcrm'), array('status' => 404));
 			}
 
-			$tag_data = $this->prepare_tag( $request );
-			$tag->update( $tag_data );
+			$tag_data = $this->prepare_tag($request);
+			$tag->update($tag_data);
 
-			return new WP_REST_Response( $tag, 200 );
-		} catch ( \Exception $e ) {
-			return new WP_Error( 'rest_tag_cannot_update', $e->getMessage(), array( 'status' => 500 ) );
+			return new WP_REST_Response($tag, 200);
+		} catch (\Exception $e) {
+			return new WP_Error('rest_tag_cannot_update', $e->getMessage(), array('status' => 500));
 		}
 	}
 
@@ -342,8 +363,9 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return bool $response Permission check result.
 	 */
-	public function update_item_permissions_check( $request ) {
-		return current_user_can( 'manage_options' );
+	public function update_item_permissions_check($request)
+	{
+		return current_user_can('manage_options');
 	}
 
 	/**
@@ -355,20 +377,21 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function delete_item( $request ) {
+	public function delete_item($request)
+	{
 		try {
-			$tag_id = $request->get_param( 'id' );
-			$tag    = Tag_Model::find( $tag_id );
+			$tag_id = $request->get_param('id');
+			$tag = Tag_Model::find($tag_id);
 
-			if ( ! $tag ) {
-				return new WP_Error( 'rest_tag_cannot_delete', __( 'Tag not found.', 'quillcrm' ), array( 'status' => 404 ) );
+			if (!$tag) {
+				return new WP_Error('rest_tag_cannot_delete', __('Tag not found.', 'quillcrm'), array('status' => 404));
 			}
 
 			$tag->delete();
 
-			return new WP_REST_Response( $tag, 200 );
-		} catch ( \Exception $e ) {
-			return new WP_Error( 'rest_tag_cannot_delete', $e->getMessage(), array( 'status' => 500 ) );
+			return new WP_REST_Response($tag, 200);
+		} catch (\Exception $e) {
+			return new WP_Error('rest_tag_cannot_delete', $e->getMessage(), array('status' => 500));
 		}
 	}
 
@@ -381,8 +404,9 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return bool $response Permission check result.
 	 */
-	public function delete_item_permissions_check( $request ) {
-		return current_user_can( 'manage_options' );
+	public function delete_item_permissions_check($request)
+	{
+		return current_user_can('manage_options');
 	}
 
 	/**
@@ -394,25 +418,26 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function deletes_item( $request ) {
+	public function deletes_item($request)
+	{
 		try {
-			$ids = $request->get_param( 'ids' );
+			$ids = $request->get_param('ids');
 
-			if ( empty( $ids ) ) {
-				return new WP_Error( 'rest_tag_cannot_delete', __( 'No tags to delete.', 'quillcrm' ), array( 'status' => 404 ) );
+			if (empty($ids)) {
+				return new WP_Error('rest_tag_cannot_delete', __('No tags to delete.', 'quillcrm'), array('status' => 404));
 			}
 
-			$tags = Tag_Model::whereIn( 'id', $ids )->get();
+			$tags = Tag_Model::whereIn('id', $ids)->get();
 
-			if ( ! $tags ) {
-				return new WP_Error( 'rest_tag_cannot_delete', __( 'Tags not found.', 'quillcrm' ), array( 'status' => 404 ) );
+			if (!$tags) {
+				return new WP_Error('rest_tag_cannot_delete', __('Tags not found.', 'quillcrm'), array('status' => 404));
 			}
 
-			Tag_Model::whereIn( 'id', $ids )->delete();
+			Tag_Model::whereIn('id', $ids)->delete();
 
-			return new WP_REST_Response( $tags, 200 );
-		} catch ( \Exception $e ) {
-			return new WP_Error( 'rest_tag_cannot_delete', $e->getMessage(), array( 'status' => 500 ) );
+			return new WP_REST_Response($tags, 200);
+		} catch (\Exception $e) {
+			return new WP_Error('rest_tag_cannot_delete', $e->getMessage(), array('status' => 500));
 		}
 	}
 
@@ -425,8 +450,9 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return bool $response Permission check result.
 	 */
-	public function deletes_item_permissions_check( $request ) {
-		return current_user_can( 'manage_options' );
+	public function deletes_item_permissions_check($request)
+	{
+		return current_user_can('manage_options');
 	}
 
 	/**
@@ -438,17 +464,18 @@ class REST_Tag_Controller extends REST_Controller {
 	 *
 	 * @return array $tag The tag model.
 	 */
-	protected function prepare_tag( $request ) {
+	protected function prepare_tag($request)
+	{
 		$tag = array(
-			'name'        => $request->get_param( 'name' ),
-			'slug'        => $request->get_param( 'slug' ),
-			'description' => $request->get_param( 'description' ),
-			'status'      => $request->get_param( 'status' ),
+			'name' => $request->get_param('name'),
+			'slug' => $request->get_param('slug'),
+			'description' => $request->get_param('description'),
+			'status' => $request->get_param('status'),
 		);
 
-		foreach ( $tag as $key => $value ) {
-			if ( ! $value ) {
-				unset( $tag[ $key ] );
+		foreach ($tag as $key => $value) {
+			if (!$value) {
+				unset($tag[$key]);
 			}
 		}
 

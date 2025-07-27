bf40750dac0b9b961a36454cdbe36e6d5c82091b
omar <omerx9988@gmail.com>
2025-07-27 10:08:37 +0300

    feat: add new campaign management page with multi-step navigation and campaign data fetching

--- src/client/pages/campaign/index-1.tsx
+++ src/client/pages/campaign/index-1.tsx
@@ -27,7 +27,6 @@ import ContactsStep from './steps/contacts';
 import ReviewStep from './steps/review';
 import { Campaign as CampaignType } from '@quillcrm/client';
 import Overview from './overview';
-import Template from '../template';
 
 const Campaign: React.FC = () => {
 	const { id, tab } = useParams<{ id: string; tab: string }>();
@@ -169,9 +168,7 @@ const Campaign: React.FC = () => {
 
 	const isOverview =
 		(campaign.status === 'schedule' && tab === 'overview') ||
-		(['processing', 'completed', 'resending'].includes(campaign.status)
-			? true
-			: false);
+		(['processing', 'completed', 'resending'].includes(campaign.status) ? true : false);
 
 	return (
 		<Provider
@@ -185,10 +182,7 @@ const Campaign: React.FC = () => {
 				...$actions,
 			}}
 		>
-			<TemplatesStep />
-			{/* {!['processing', 'completed', 'resending'].includes(
-				campaign.status
-			) &&
+			{!['processing', 'completed', 'resending'].includes(campaign.status) &&
 				tab !== 'overview' && (
 					<Tabs
 						defaultActiveKey="information"
@@ -203,7 +197,7 @@ const Campaign: React.FC = () => {
 						}}
 					/>
 				)}
-			{isOverview && <Overview />} */}
+			{isOverview && <Overview />}
 		</Provider>
 	);
 };

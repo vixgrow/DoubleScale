bcdcb619c4d2cfa1d1f674cf3a645982d8214dc7
omar <omerx9988@gmail.com>
2025-08-05 14:02:07 +0300

    fix: add margin class to BreadcrumbItem for consistent spacing

--- src/components/breadcrumb/index.tsx
+++ src/components/breadcrumb/index.tsx
@@ -20,7 +20,7 @@ export default function BreadcrumbComponent({
 			<BreadcrumbList>
 				{items.map((item, index) => (
 					<>
-						<BreadcrumbItem key={index}>
+						<BreadcrumbItem key={index} className="mb-0">
 							{index === items.length - 1 ? (
 								<BreadcrumbPage className="text-primary">
 									{item.label}

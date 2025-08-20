bd5404b5522be3cae3737d78737ee1e370581a1e
omar <omerx9988@gmail.com>
2025-08-20 13:57:25 +0300

    feat: add new layout options including single column, two columns (equal and varied), three columns, and four columns to enhance layout flexibility in the builder

--- src/builder/data/layouts.tsx
+++ src/builder/data/layouts.tsx
@@ -2,31 +2,37 @@ import { __ } from '@wordpress/i18n';
 
 export const layoutsStyles = [
 	{
+		id: 'single-column',
 		name: __('Single Column', 'quillcrm'),
 		number: [1],
 		value: 'single-column',
 	},
 	{
+		id: 'two-columns-equal',
 		name: __('Two Columns', 'quillcrm'),
 		number: [1, 1],
 		value: 'two-columns',
 	},
 	{
+		id: 'two-columns-2-1',
 		name: __('Two Columns', 'quillcrm'),
 		number: [2, 1],
 		value: 'two-columns',
 	},
 	{
+		id: 'two-columns-1-2',
 		name: __('Two Columns', 'quillcrm'),
 		number: [1, 2],
 		value: 'two-columns',
 	},
 	{
+		id: 'three-columns',
 		name: __('Three Columns', 'quillcrm'),
 		number: [1, 1, 1],
 		value: 'three-columns',
 	},
 	{
+		id: 'four-columns',
 		name: __('Four Columns', 'quillcrm'),
 		number: [1, 1, 1, 1],
 		value: 'four-columns',

5ac139ab67b64b737abd56340be72982df43efee
emy185 <emyy729@gmail.com>
2025-08-21 17:00:23 +0200

    feat: enhance MenuBlock component with new properties for menu items; improve editor and renderer for better menu management and user experience

--- src/builder/blocks/basic/MenuBlock/index.tsx
+++ src/builder/blocks/basic/MenuBlock/index.tsx
@@ -9,10 +9,30 @@ import { MenuBlockIcon } from '@quillcrm/components';
 import { MenuBlockRenderer } from './Renderer';
 import { MenuBlockEditor } from './Editor';
 
-export interface MenuBlockProps {
-	content: string;
+export interface MenuItem {
+	id: string;
+	name: string;
+	link: string;
 	fontSize: number;
 	color: string;
+	fontFamily: string;
+	bold: boolean;
+	italic: boolean;
+	underline: boolean;
+	strikethrough: boolean;
+	backgroundColor: string;
+	borderRadius: string;
+	letterSpacing: string;
+}
+
+export interface MenuBlockProps {
+	menuItems: MenuItem[];
+	padding: {
+		top: number;
+		right: number;
+		bottom: number;
+		left: number;
+	};
 	align: string;
 }
 
@@ -21,9 +41,44 @@ const MenuBlock = {
 	name: __('Menu', 'quillcrm'),
 	icon: MenuBlockIcon,
 	defaultProps: {
-		content: 'Your text here',
-		fontSize: 16,
-		color: '#333',
+		menuItems: [
+			{
+				id: '1',
+				name: 'Menu Item 01',
+				link: '#',
+				fontSize: 16,
+				color: '#333',
+				fontFamily: 'Arial',
+				bold: false,
+				italic: false,
+				underline: false,
+				strikethrough: false,
+				backgroundColor: 'transparent',
+				borderRadius: '0',
+				letterSpacing: '0px',
+			},
+			{
+				id: '2',
+				name: 'Menu Item 02',
+				link: '#',
+				fontSize: 16,
+				color: '#333',
+				fontFamily: 'Arial',
+				bold: false,
+				italic: false,
+				underline: false,
+				strikethrough: false,
+				backgroundColor: 'transparent',
+				borderRadius: '0',
+				letterSpacing: '0px',
+			},
+		],
+		padding: {
+			top: 4,
+			right: 8,
+			bottom: 4,
+			left: 8,
+		},
 		align: 'center',
 	} as MenuBlockProps,
 	Renderer: MenuBlockRenderer,

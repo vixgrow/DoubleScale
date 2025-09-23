a2411ac0dbd1b0ec966f6b086537ffcc66198c55
emy185 <emyy729@gmail.com>
2025-09-23 13:53:18 +0200

    feat: enhance email builder selection dialog by adding DialogPortal and DialogOverlay for improved modal functionality

--- src/client/pages/campaign/steps/templates/email-builder-selection/index.tsx
+++ src/client/pages/campaign/steps/templates/email-builder-selection/index.tsx
@@ -12,9 +12,10 @@ import {
 	DialogDescription,
 	DialogHeader,
 	DialogTitle,
+	DialogOverlay,
+	DialogPortal,
 } from '@/components/ui/dialog';
 import { EnvelopeIcon, RepeatIcon } from '@/components';
-import ArrowRightIcon from '@/components/icons/arrow-right';
 
 interface CampaignTypesProps {
 	setVisible: (visible: boolean) => void;
@@ -54,50 +55,56 @@ const EmailBuilderSelection: React.FC<CampaignTypesProps> = ({
 	];
 	return (
 		<Dialog open={visible} onOpenChange={() => setVisible(!visible)}>
-			<DialogContent className="max-w-[840px] w-full mx-auto">
-				<DialogHeader className="text-center sm:text-center">
-					<DialogTitle className="text-3xl font-bold mb-1">
-						{__(
-							'Choose how you`d like to build your Email',
-							'quillcrm'
-						)}
-					</DialogTitle>
-					<DialogDescription className="text-foreground">
-						{__(
-							'Select one of the Type to start creating your own template',
-							'quillcrm'
-						)}
-					</DialogDescription>
-				</DialogHeader>
-				<div className="flex gap-4">
-					{campaignTypesRows.map((campaignType) => (
-						<div
-							className="flex flex-col justify-between items-center py-6 px-4 border border-gray-200 rounded-lg cursor-pointer hover:bg-secondary-background hover:border-primary hover:transition-all hover:duration-300"
-							key={campaignType.type}
-							onClick={() => {
-								setVisible(false);
-							}}
-						>
-							<div className="bg-gradient-to-r from-[#1E3A8A] to-[#3B82F6] text-primary-foreground p-3 rounded-xl">
-								{campaignType.icon}
-							</div>
-							<div className="flex items-center gap-2 w-full justify-center">
-								<p className="font-semibold text-foreground text-sm my-2">
-									{campaignType.label}
+			<DialogPortal>
+				<DialogOverlay style={{ zIndex: 9999999 }} />
+				<DialogContent
+					className="max-w-[840px] w-full mx-auto"
+					style={{ zIndex: 9999999 }}
+				>
+					<DialogHeader className="text-center sm:text-center">
+						<DialogTitle className="text-3xl font-bold mb-1">
+							{__(
+								'Choose how you`d like to build your Email',
+								'quillcrm'
+							)}
+						</DialogTitle>
+						<DialogDescription className="text-foreground">
+							{__(
+								'Select one of the Type to start creating your own template',
+								'quillcrm'
+							)}
+						</DialogDescription>
+					</DialogHeader>
+					<div className="flex gap-4">
+						{campaignTypesRows.map((campaignType) => (
+							<div
+								className="flex flex-col justify-between items-center py-6 px-4 border border-gray-200 rounded-lg cursor-pointer hover:bg-secondary-background hover:border-primary hover:transition-all hover:duration-300"
+								key={campaignType.type}
+								onClick={() => {
+									setVisible(false);
+								}}
+							>
+								<div className="bg-gradient-to-r from-[#1E3A8A] to-[#3B82F6] text-primary-foreground p-3 rounded-xl">
+									{campaignType.icon}
+								</div>
+								<div className="flex items-center gap-2 w-full justify-center">
+									<p className="font-semibold text-foreground text-sm my-2">
+										{campaignType.label}
+									</p>
+									{campaignType.beta && (
+										<span className="text-xs text-primary bg-secondary-background rounded-full px-2 py-1 font-semibold">
+											{__('Beta', 'quillcrm')}
+										</span>
+									)}
+								</div>
+								<p className="text-foreground text-sm text-center">
+									{campaignType.description}
 								</p>
-								{campaignType.beta && (
-									<span className="text-xs text-primary bg-secondary-background rounded-full px-2 py-1 font-semibold">
-										{__('Beta', 'quillcrm')}
-									</span>
-								)}
 							</div>
-							<p className="text-foreground text-sm text-center">
-								{campaignType.description}
-							</p>
-						</div>
-					))}
-				</div>
-			</DialogContent>
+						))}
+					</div>
+				</DialogContent>
+			</DialogPortal>
 		</Dialog>
 	);
 };

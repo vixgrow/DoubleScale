7454376e1ab54c69c8db5d2238f1f6eb4cfc96e9
emy185 <emyy729@gmail.com>
2025-10-06 15:31:38 +0200

    refactor: reorganize EmailSettings layout for improved user experience and integrate slider inputs for email limits

--- src/client/pages/settings/email/index.tsx
+++ src/client/pages/settings/email/index.tsx
@@ -38,45 +38,65 @@ const EmailSettings: React.FC<EmailSettingsProps> = ({
     };
     return (
         <div className="email-settings qcrm-fields">
-            <Field
-                label={__('From Name', 'quillcrm')}
-                value={from_name || ConfigAPI.getBlogName()}
-                onChange={(value) => handleFieldChange('from_name', value)}
-                type="text"
-            />
-            <Field
-                label={__('From Email', 'quillcrm')}
-                value={from_email || ConfigAPI.getBlogName()}
-                onChange={(value) => handleFieldChange('from_email', value)}
-                type="email"
-            />
-            <Field
-                label={__('Reply To', 'quillcrm')}
-                value={reply_to || ConfigAPI.getBlogName()}
-                onChange={(value) => handleFieldChange('reply_to', value)}
-                type="email"
-            />
-            <Field
-                label={__('Email Footer', 'quillcrm')}
-                value={email_footer}
-                onChange={(value) => handleFieldChange('email_footer', value)}
-                type="textarea"
-            />
-            <Field
-                label={__('Max Emails in Second', 'quillcrm')}
-                value={max_in_second}
-                onChange={(value) => handleFieldChange('max_in_second', value)}
-                type="number"
-            />
-            <Field
-                label={__('Max Emails in Day', 'quillcrm')}
-                value={max_in_day}
-                onChange={(value) => handleFieldChange('max_in_day', value)}
-                type="number"
-            />
+            <div className="text-[#09090B] font-semibold text-2xl">
+                {__('Email', 'quillcrm')}
+            </div>
+            <div className="flex gap-5 items-center mb-3">
+                <Field
+                    label={__('From Name', 'quillcrm')}
+                    value={from_name || ConfigAPI.getBlogName()}
+                    onChange={(value) => handleFieldChange('from_name', value)}
+                    type="text"
+                />
+                <Field
+                    label={__('From Email', 'quillcrm')}
+                    value={from_email || ConfigAPI.getBlogName()}
+                    onChange={(value) => handleFieldChange('from_email', value)}
+                    type="email"
+                />
+                <Field
+                    label={__('Reply To', 'quillcrm')}
+                    value={reply_to || ConfigAPI.getBlogName()}
+                    onChange={(value) => handleFieldChange('reply_to', value)}
+                    type="email"
+                />
+            </div>
+            <div className="flex gap-5 items-start w-full">
+                <div className="w-full flex flex-col gap-5">
+                    <Field
+                        label={__('Max Emails in Second', 'quillcrm')}
+                        value={max_in_second}
+                        onChange={(value) =>
+                            handleFieldChange('max_in_second', value)
+                        }
+                        type="slider"
+                        min={0}
+                        max={1500}
+                    />
+                    <Field
+                        label={__('Max Emails in Day', 'quillcrm')}
+                        value={max_in_day}
+                        onChange={(value) =>
+                            handleFieldChange('max_in_day', value)
+                        }
+                        type="slider"
+                        min={0}
+                        max={1000}
+                    />
+                </div>
+                <div className="w-full">
+                    <Field
+                        label={__('Email Footer', 'quillcrm')}
+                        value={email_footer}
+                        onChange={(value) =>
+                            handleFieldChange('email_footer', value)
+                        }
+                        type="textarea"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
 
 export default EmailSettings;
-

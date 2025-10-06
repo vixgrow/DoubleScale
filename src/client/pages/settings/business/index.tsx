ee077238675d45a1d86b2207b51fdd3bb5ceeb89
emy185 <emyy729@gmail.com>
2025-10-06 14:35:27 +0200

    feat: add business title to BusinessSettings component for improved UI clarity

--- src/client/pages/settings/business/index.tsx
+++ src/client/pages/settings/business/index.tsx
@@ -31,6 +31,7 @@ const BusinessSettings: React.FC<BusinessSettingsProps> = ({
     };
     return (
         <div className="business-settings qcrm-fields">
+            <div className="text-[#09090B] font-semi text-2xl">{__('Business', 'quillcrm')}</div>
             <Field
                 label={__('Business Name', 'quillcrm')}
                 value={business_name || ConfigAPI.getBlogName()}

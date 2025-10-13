27e2d84d39a86d7fe649fc09cc0821c3e87cde06
ahmedgalal2001 <ahmedgalalmohamed912@gmail.com>
2025-10-13 14:39:08 +0300

    feat: add Forms_Metadata class and enhance dynamic fields registration with form metadata tags for improved automation capabilities

--- includes/merge-tags/fluentforms/class-dynamic-fields-registration.php
+++ includes/merge-tags/fluentforms/class-dynamic-fields-registration.php
@@ -11,6 +11,8 @@
 namespace QuillCRM\Merge_Tags\FluentForms;
 
 use QuillCRM\Managers\Merge_Tags_Manager;
+use QuillCRM\Merge_Tags\Forms\Forms_Field;
+use QuillCRM\Merge_Tags\Forms\Forms_Metadata;
 
 /**
  * Dynamic FluentForms Fields Registration
@@ -22,8 +24,9 @@ class Dynamic_Fields_Registration {
 	/**
 	 * Constructor
 	 */
-	public function __construct( $fields ) {
-		$this->register_fields_from_data( $fields );
+	public function __construct( $fields, $slug ) {
+		$this->register_form_metadata_tags( $slug );
+		$this->register_fields_from_data( $fields, $slug );
 	}
 
 	/**
@@ -49,6 +52,31 @@ class Dynamic_Fields_Registration {
 
 			$merge_tag = new FluentForms_Field( $field_name, $field_label );
 			Merge_Tags_Manager::instance()->register( $merge_tag );
+		}
+	}
+
+	/**
+	 * Register form metadata merge tags (form_title, form_id)
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $slug Form slug
+	 *
+	 * @return void
+	 */
+	public function register_form_metadata_tags( $slug ) {
+		// Register form_title merge tag
+		$existing_title_tag = Merge_Tags_Manager::instance()->get_merge_tag( $slug, 'form_title' );
+		if ( ! $existing_title_tag ) {
+			$title_merge_tag = new Forms_Metadata( 'form_title', 'Form Title', $slug );
+			Merge_Tags_Manager::instance()->register( $title_merge_tag );
+		}
+
+		// Register form_id merge tag
+		$existing_id_tag = Merge_Tags_Manager::instance()->get_merge_tag( $slug, 'form_id' );
+		if ( ! $existing_id_tag ) {
+			$id_merge_tag = new Forms_Metadata( 'form_id', 'Form ID', $slug );
+			Merge_Tags_Manager::instance()->register( $id_merge_tag );
 		}
 	}
 }

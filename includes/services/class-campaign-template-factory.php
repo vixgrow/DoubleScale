930620ef56a20c5ef41a0a93b1ebb875fda60915
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-10-14 10:09:13 +0300

    refactor: enhance logging for template processing in campaign template factory
    - Updated the Campaign_Template_Factory class to improve logging during template processing.
    - Reinstated logging for both the start and successful completion of template processing, providing better insights into the campaign workflow.
    - This change aids in debugging and monitoring the template handling process.

--- includes/services/class-campaign-template-factory.php
+++ includes/services/class-campaign-template-factory.php
@@ -65,22 +65,22 @@ class Campaign_Template_Factory
         $template_ids = array();
 
         foreach ($templates_data as $index => $template_data) {
-            // quillcrm_get_logger()->info('Processing template', [
-            //     'index' => $index,
-            //     'template_data' => $template_data,
-            //     'campaign_type' => $campaign_type,
-            //     'code' => 'template_processing_start'
-            // ]);
+            quillcrm_get_logger()->info('Processing template', [
+                'index' => $index,
+                'template_data' => $template_data,
+                'campaign_type' => $campaign_type,
+                'code' => 'template_processing_start'
+            ]);
             
             $template_processor = $this->get_template_processor($campaign_type);
             $template = $template_processor->process($template_data);
 
             if ($template) {
                 $template_ids[] = $template->id;
-                // quillcrm_get_logger()->info('Template processed successfully', [
-                //     'template_id' => $template->id,
-                //     'code' => 'template_processing_success'
-                // ]);
+                quillcrm_get_logger()->info('Template processed successfully', [
+                    'template_id' => $template->id,
+                    'code' => 'template_processing_success'
+                ]);
             } else {
                 quillcrm_get_logger()->error('Template processing failed', [
                     'template_data' => $template_data,

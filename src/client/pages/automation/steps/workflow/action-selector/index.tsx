86ebe2dcae6dba4f608dfc6c5e18ad6223c3e14b
emy185 <emyy729@gmail.com>
2025-10-14 15:29:14 +0200

    feat: update ActionSelector to use dialog UI with category selection and improved layout for automation actions

--- src/client/pages/automation/steps/workflow/action-selector/index.tsx
+++ src/client/pages/automation/steps/workflow/action-selector/index.tsx
@@ -5,17 +5,34 @@ import { __ } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
 
 /**
- * External dependencies
- */
-import { Button, Flex, Typography, Tabs, Modal } from 'antd';
-import { map } from 'lodash';
-
-/**
  * Internal dependencies
  */
+import ActionsGroupRender from './actions-group-render';
+import ActionSelectorCard from './action-selector-card';
 import './style.scss';
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+	DialogFooter,
+	DialogOverlay,
+	DialogDescription,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import ConfigAPI from '@quillcrm/config';
-import type { ActionsGroup } from '@quillcrm/config';
+//@ts-ignore
+import crm from '../../../../../../../assets/images/crm/crm.png';
+//@ts-ignore
+import forms from '../../../../../../../assets/images/forms/forms.png';
+//@ts-ignore
+import woocommerce from '../../../../../../../assets/images/woocoomerce/woo-icon.png';
+//@ts-ignore
+import wpusers from '../../../../../../../assets/images/wordpress/wordpress-icon.png';
+//@ts-ignore
+import lms from '../../../../../../../assets/images/lms/lms.png';
+//@ts-ignore
+import data from '../../../../../../../assets/images/send-data/data.png';
 
 interface ActionSelectorProps {
 	value: string;
@@ -33,18 +50,8 @@ const ActionSelector: React.FC<ActionSelectorProps> = ({
 	onClose,
 }) => {
 	const [isSaving, setIsSaving] = useState(false);
+	const [selectedCategory, setSelectedCategory] = useState('crm');
 	const automationActions = ConfigAPI.getAutomationActions();
-	const automationActionsTabs = map(automationActions, (trigger, index) => ({
-		key: index,
-		label: trigger.label,
-		children: (
-			<ActionsGroupRender
-				groups={trigger.groups}
-				onChange={(value) => onChange(value)}
-				value={value}
-			/>
-		),
-	}));
 
 	const saveStep = async () => {
 		setIsSaving(true);
@@ -59,68 +66,73 @@ const ActionSelector: React.FC<ActionSelectorProps> = ({
 		}
 	};
 
+	const categoryData = {
+		'crm': {
+			image: crm,
+			description: __('Automate your CRM workflows and tasks', 'quillcrm')
+		},
+		'lms': {
+			image: lms,
+			description: __('Trigger actions when forms are submitted', 'quillcrm')
+		},
+		'woocommerce': {
+			image: woocommerce,
+			description: __('E-commerce and order automation', 'quillcrm')
+		},
+		'wp': {
+			image: wpusers,
+			description: __('WordPress user and content automation', 'quillcrm')
+		},
+		'send_data': {
+			image: data,
+			description: __('Send data to external services', 'quillcrm')
+		},
+		'email': {
+			image: forms,
+			description: __('Send email to users', 'quillcrm')
+		}
+	};
+
+	const currentCategoryData = automationActions[selectedCategory];
+
 	return (
-		<Modal
-			title={__('Select Action', 'quillcrm')}
-			open={visible}
-			onOk={saveStep}
-			onCancel={() => onClose()}
-			confirmLoading={isSaving}
-			style={{ minWidth: '800px' }}
-			closable={false}
-		>
-			<div className="qcrm-fields" style={{ marginBottom: '20px' }}>
-				<div className="qcrm-field">
-					<div className="qcrm-field-input">
-						<Tabs
-							defaultActiveKey="0"
-							tabPosition="left"
-							items={automationActionsTabs}
-						/>
+		<Dialog open={visible} onOpenChange={(open) => !open && onClose()}>
+			<DialogOverlay className="z-[150200]" />
+			<DialogContent className="z-[150200] h-[90vh] overflow-y-auto max-w-[1000px]">
+				<DialogHeader>
+					<DialogTitle>{__('Action Library', 'quillcrm')}</DialogTitle>
+					<DialogDescription className='mt-1'>{__('Select an action to add to your workflow', 'quillcrm')}</DialogDescription>
+				</DialogHeader>
+				<div className="qcrm-fields">
+					<div className="qcrm-field">
+						<div className="flex h-full gap-5">
+							<div className="w-1/2">
+								<ActionSelectorCard
+									automationActions={automationActions}
+									selectedCategory={selectedCategory}
+									setSelectedCategory={setSelectedCategory}
+									categoryData={categoryData}
+								/>
+							</div>
+							<div className="w-1/2">
+								<ActionsGroupRender
+									groups={currentCategoryData?.groups || []}
+									onChange={(value) => onChange(value)}
+									value={value}
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</Modal>
-	);
-};
-
-const ActionsGroupRender: React.FC<{
-	groups: ActionsGroup[];
-	onChange: (value: string) => void;
-	value: string;
-}> = ({ groups, onChange, value }) => {
-	return (
-		<Flex gap={20} wrap vertical={true}>
-			{map(groups, (group, key) => (
-				<div key={key} className="qcrm-automation-actions-group">
-					<Typography.Paragraph
-						strong
-						className="qcrm-automation-actions-group__label"
-						style={{ marginBottom: '10px' }}
-					>
-						{group.label}
-					</Typography.Paragraph>
-					<Flex
-						className="qcrm-automation-actions-group__actions"
-						gap={10}
-						wrap
-					>
-						{map(group.actions, (action, key) => {
-							return (
-								<Button
-									key={key}
-									onClick={() => onChange(key)}
-									type={value === key ? 'primary' : 'default'}
-									disabled={group.is_disabled}
-								>
-									{action.label}
-								</Button>
-							);
-						})}
-					</Flex>
-				</div>
-			))}
-		</Flex>
+				<DialogFooter>
+					<Button onClick={saveStep} disabled={isSaving} size="xl" variant="gradient" className="w-full mt-4">
+						{isSaving
+							? __('Saving...', 'quillcrm')
+							: __('Save', 'quillcrm')}
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
 	);
 };
 

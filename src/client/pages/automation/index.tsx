ce0773b8aa027d96d584bd2bc904a72262052c88
emy185 <emyy729@gmail.com>
2025-10-22 13:29:37 +0200

    refactor(node-actions-dropdown): enhance pointer events and improve dialog overlay for better user interaction

--- src/client/pages/automation/index.tsx
+++ src/client/pages/automation/index.tsx
@@ -141,7 +141,7 @@ const Automation: React.FC = () => {
 				}}
 			>
 				<DialogContent
-					className="z-[150000] w-screen h-screen max-w-none gap-0 overflow-hidden bg-white rounded-none shadow-none"
+					className="z-[150000] w-screen h-screen max-w-none gap-0 overflow-y-auto overflow-x-hidden bg-white rounded-none shadow-none"
 					style={{
 						paddingTop: '10px',
 						paddingLeft: '0px',

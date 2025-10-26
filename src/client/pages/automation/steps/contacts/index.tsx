ac50307aba9b74c601089e06ef9082b23631be07
emy185 <emyy729@gmail.com>
2025-10-26 12:10:52 +0200

    refactor(contacts-list): adjust container height for improved layout and scrolling behavior

--- src/client/pages/automation/steps/contacts/index.tsx
+++ src/client/pages/automation/steps/contacts/index.tsx
@@ -90,7 +90,7 @@ const ContactsList: React.FC = () => {
 	};
 
 	return (
-		<div className="qcrm-contacts-list px-5 overflow-y-auto">
+		<div className="qcrm-contacts-list px-5 overflow-y-auto h-screen">
 			<div className="qcrm-contacts-list__table">
 				<DataTable
 					columns={columns}

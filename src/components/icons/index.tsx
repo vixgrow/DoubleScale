e6f46e313cc18015071cf7654aeca0d7670c90df
emy185 <emyy729@gmail.com>
2025-11-06 12:14:49 +0200

    feat(Icons): add GradientSendSMSIcon and GradientMergeTagsIcon, and update icon exports for consistency

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -165,4 +165,7 @@ export { default as ScheduledEmailsIcon } from './scheduled-emails';
 export { default as ProcessingEmailsIcon } from './processing-emails';
 export { default as ClickToOpenRateIcon } from './click-to-open-rate';
 export { default as DeliveryRateIcon } from './delivery-rate';
-export { default as ReadRateIcon } from './read-rate';export { default as ABSplitIcon } from './ab-split';
+export { default as ReadRateIcon } from './read-rate';
+export { default as ABSplitIcon } from './ab-split';
+export { default as GradientSendSMSIcon } from './gradient-send-sms';
+export { default as GradientMergeTagsIcon } from './gradient-merge-tags';

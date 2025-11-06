10413f533d63411c93296b2ab5ce764172b9df23
emy185 <emyy729@gmail.com>
2025-11-06 14:14:51 +0200

    refactor(icons): remove unnecessary newline in index.tsx for cleaner exports

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -183,7 +183,6 @@ export { default as ProcessingEmailsIcon } from './processing-emails';
 export { default as ClickToOpenRateIcon } from './click-to-open-rate';
 export { default as DeliveryRateIcon } from './delivery-rate';
 export { default as ReadRateIcon } from './read-rate';
-
 export { default as ABSplitIcon } from './ab-split';
 export { default as SetUpInfoIcon } from './set-up-info';
 export { default as SaveAsTemplateIcon } from './save-as-template';

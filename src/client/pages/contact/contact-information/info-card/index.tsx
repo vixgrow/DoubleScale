ce9d5d7cc9bf333d801762c76c6ceefec246dfc7
emy185 <emyy729@gmail.com>
2025-11-06 13:17:42 +0200

    feat(InfoCard): add OutlinedCustomFieldsIcon to enhance visual representation of group information

--- src/client/pages/contact/contact-information/info-card/index.tsx
+++ src/client/pages/contact/contact-information/info-card/index.tsx
@@ -496,6 +496,7 @@ const InfoCard: React.FC = () => {
                                     <CardHeader className="px-4 py-2 border-b bg-[#F8F8F8] rounded-t-xl">
                                         <CardTitle className="flex items-center justify-between font-medium text-lg">
                                             <div className="flex items-center gap-2">
+                                                <OutlinedCustomFieldsIcon width={20} height={20} />
                                                 {group.name}
                                             </div>
                                             <Button

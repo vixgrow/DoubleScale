79ba33d13cb7e47d6cdd94347433e14e93930ef7
ahmedgalal2001 <ahmedgalalmohamed912@gmail.com>
2025-11-16 16:25:31 +0200

    refactor(coupon): streamline code formatting and improve readability

--- includes/automations/actions/woocommerce/class-create-coupon.php
+++ includes/automations/actions/woocommerce/class-create-coupon.php
@@ -20,18 +20,8 @@ use QuillCRM\Models\Automation_Contact_Model;
 /**
  * Create Coupon Action
  */
-class Create_Coupon extends Action {
-
-
-
-
-
-
-
-
-
-
-
+class Create_Coupon extends Action
+{
 
 
 
@@ -82,12 +72,13 @@ class Create_Coupon extends Action {
 	 *
 	 * @return bool
 	 */
-	public function process_action( Automation_Model $automation, Automation_Step_Model $step, Automation_Contact_Model $automation_contact ) {
+	public function process_action(Automation_Model $automation, Automation_Step_Model $step, Automation_Contact_Model $automation_contact)
+	{
 		try {
 			// Check if WooCommerce classes exist
-			if ( ! class_exists( 'WC_Coupon' ) ) {
+			if (! class_exists('WC_Coupon')) {
 				quillcrm_get_logger()->error(
-					__( 'WooCommerce plugin is not active. Cannot create coupon.', 'quillcrm' ),
+					__('WooCommerce plugin is not active. Cannot create coupon.', 'quillcrm'),
 					array(
 						'code'          => 'woocommerce_plugin_inactive',
 						'automation_id' => $automation->id,
@@ -97,18 +88,18 @@ class Create_Coupon extends Action {
 				return false;
 			}
 
-			$coupon_expiry_date     = $step->get_setting( 'coupon_expiry_date' );
-			$discount_type          = $step->get_setting( 'discount_type' );
-			$coupon_prefix          = $step->get_setting( 'coupon_prefix' ) ?? '';
-			$is_free_shipping       = $step->get_setting( 'is_free_shipping' ) ?? false;
-			$title                  = $step->get_setting( 'title' ) ?? '';
-			$usage_limit_per_coupon = $step->get_setting( 'usage_limit_per_coupon' ) ?? 0;
-			$limit_usage_to_x_items = $step->get_setting( 'limit_usage_to_x_items' ) ?? 0;
-			$usage_limit_per_user   = $step->get_setting( 'usage_limit_per_user' ) ?? 0;
+			$coupon_expiry_date     = $step->get_setting('coupon_expiry_date');
+			$discount_type          = $step->get_setting('discount_type');
+			$coupon_prefix          = $step->get_setting('coupon_prefix') ?? '';
+			$is_free_shipping       = $step->get_setting('is_free_shipping') ?? false;
+			$title                  = $step->get_setting('title') ?? '';
+			$usage_limit_per_coupon = $step->get_setting('usage_limit_per_coupon') ?? 0;
+			$limit_usage_to_x_items = $step->get_setting('limit_usage_to_x_items') ?? 0;
+			$usage_limit_per_user   = $step->get_setting('usage_limit_per_user') ?? 0;
 
-			if ( empty( $discount_type ) ) {
+			if (empty($discount_type)) {
 				quillcrm_get_logger()->warning(
-					__( 'Discount type not configured for coupon creation', 'quillcrm' ),
+					__('Discount type not configured for coupon creation', 'quillcrm'),
 					array(
 						'code'          => 'woocommerce_discount_type_missing',
 						'automation_id' => $automation->id,
@@ -118,33 +109,33 @@ class Create_Coupon extends Action {
 				return false;
 			}
 
-			$coupon_code              = $this->generate_dynamic_coupon_code( $coupon_prefix );
-			$discount_type_and_amount = $this->get_discount_type_and_amount( $discount_type['type'], $discount_type['amount'] );
-			$expiry_date              = $this->get_expiry_date( $coupon_expiry_date['type'], $coupon_expiry_date['value'] );
+			$coupon_code              = $this->generate_dynamic_coupon_code($coupon_prefix);
+			$discount_type_and_amount = $this->get_discount_type_and_amount($discount_type['type'], $discount_type['amount']);
+			$expiry_date              = $this->get_expiry_date($coupon_expiry_date['type'], $coupon_expiry_date['value']);
 
 			$coupon = new \WC_Coupon();
-			$coupon->set_code( $coupon_code );
-			$coupon->set_amount( $discount_type_and_amount['amount'] );
-			$coupon->set_discount_type( $discount_type_and_amount['type'] );
-			$coupon->set_date_expires( $expiry_date );
-			$coupon->set_description( $title );
-			$coupon->set_free_shipping( $is_free_shipping );
-			$coupon->set_usage_limit( $usage_limit_per_coupon );
-			$coupon->set_limit_usage_to_x_items( $limit_usage_to_x_items );
-			$coupon->set_usage_limit_per_user( $usage_limit_per_user );
+			$coupon->set_code($coupon_code);
+			$coupon->set_amount($discount_type_and_amount['amount']);
+			$coupon->set_discount_type($discount_type_and_amount['type']);
+			$coupon->set_date_expires($expiry_date);
+			$coupon->set_description($title);
+			$coupon->set_free_shipping($is_free_shipping);
+			$coupon->set_usage_limit($usage_limit_per_coupon);
+			$coupon->set_limit_usage_to_x_items($limit_usage_to_x_items);
+			$coupon->set_usage_limit_per_user($usage_limit_per_user);
 			$coupon->save();
 
 			// automation contact data
-			$existing_coupons   = $automation_contact->get_data( 'coupon_codes', array() );
+			$existing_coupons   = $automation_contact->get_data('coupon_codes', array());
 			$existing_coupons[] = array(
 				'code'    => $coupon_code,
 				'step_id' => $step->id,
 			);
 
-			$automation_contact->set_data( array( 'coupon_codes' => $existing_coupons ) );
+			$automation_contact->set_data(array('coupon_codes' => $existing_coupons));
 
 			quillcrm_get_logger()->info(
-				__( 'WooCommerce coupon created successfully', 'quillcrm' ),
+				__('WooCommerce coupon created successfully', 'quillcrm'),
 				array(
 					'code'          => 'woocommerce_coupon_created',
 					'coupon_code'   => $coupon_code,
@@ -154,9 +145,9 @@ class Create_Coupon extends Action {
 			);
 
 			return true;
-		} catch ( \Exception $e ) {
+		} catch (\Exception $e) {
 			quillcrm_get_logger()->error(
-				__( 'Failed to create WooCommerce coupon', 'quillcrm' ),
+				__('Failed to create WooCommerce coupon', 'quillcrm'),
 				array(
 					'code'          => 'woocommerce_coupon_creation_failed',
 					'error'         => $e->getMessage(),
@@ -178,9 +169,10 @@ class Create_Coupon extends Action {
 	 *
 	 * @return string
 	 */
-	public function generate_dynamic_coupon_code( $prefix = '' ) {
+	public function generate_dynamic_coupon_code($prefix = '')
+	{
 
-		$random_code = strtoupper( wp_generate_password( 6, false ) );
+		$random_code = strtoupper(wp_generate_password(6, false));
 
 		$coupon_code = $prefix . $random_code;
 
@@ -197,14 +189,15 @@ class Create_Coupon extends Action {
 	 *
 	 * @return WC_DateTime|string
 	 */
-	public function get_expiry_date( $type, $value ) {
-		switch ( $type ) {
+	public function get_expiry_date($type, $value)
+	{
+		switch ($type) {
 			case 'never':
 				return '';
 			case 'days':
-				return date( 'Y-m-d', strtotime( "+{$value} days" ) );
+				return date('Y-m-d', strtotime("+{$value} days"));
 			case 'date':
-				return new \WC_DateTime( $value );
+				return new \WC_DateTime($value);
 		}
 
 		return '';
@@ -218,44 +211,45 @@ class Create_Coupon extends Action {
 	 * @param string $type Type.
 	 * @param string $amount Amount.
 	 */
-	public function get_discount_type_and_amount( $type, $amount ) {
+	public function get_discount_type_and_amount($type, $amount)
+	{
 
-		if ( empty( $type ) || empty( $amount ) ) {
+		if (empty($type) || empty($amount)) {
 			return array(
 				'type'   => 'fixed_cart',
 				'amount' => 0,
 			);
 		}
 
-		if ( ! is_numeric( $amount ) ) {
+		if (! is_numeric($amount)) {
 			return array(
 				'type'   => 'fixed_cart',
 				'amount' => 0,
 			);
 		}
 
-		if ( $amount < 0 ) {
+		if ($amount < 0) {
 			return array(
 				'type'   => 'fixed_cart',
 				'amount' => 0,
 			);
 		}
 
-		if ( $type === 'fixed_cart' ) {
+		if ($type === 'fixed_cart') {
 			return array(
 				'type'   => 'fixed_cart',
 				'amount' => $amount,
 			);
 		}
 
-		if ( $type === 'fixed_product' ) {
+		if ($type === 'fixed_product') {
 			return array(
 				'type'   => 'fixed_product',
 				'amount' => $amount,
 			);
 		}
 
-		if ( $type === 'percent' ) {
+		if ($type === 'percent') {
 			return array(
 				'type'   => 'percent',
 				'amount' => $amount > 100 ? 100 : $amount,
@@ -270,56 +264,57 @@ class Create_Coupon extends Action {
 	 *
 	 * @return array
 	 */
-	public function get_fields() {
+	public function get_fields()
+	{
 		return array(
 			'general' => array(
 				'type'   => 'group',
-				'label'  => __( 'General', 'quillcrm' ),
+				'label'  => __('General', 'quillcrm'),
 				'fields' => array(
 					'title'              => array(
 						'type'        => 'text',
-						'label'       => __( 'Coupon Title', 'quillcrm' ),
-						'helperText'  => __( 'This dynamic coupon can be used in emails or other actions using merge tag: {{coupon:dynamic_id_STEP_ID}}', 'quillcrm' ),
-						'description' => __( 'After creating this action step, you can use the generated merge tag to reference this coupon in emails and other actions.', 'quillcrm' ),
+						'label'       => __('Coupon Title', 'quillcrm'),
+						'helperText'  => __('This dynamic coupon can be used in emails or other actions using merge tag: {{coupon:dynamic_id_STEP_ID}}', 'quillcrm'),
+						'description' => __('After creating this action step, you can use the generated merge tag to reference this coupon in emails and other actions.', 'quillcrm'),
 					),
 					'coupon_prefix'      => array(
 						'type'  => 'text',
-						'label' => __( 'Coupon Prefix', 'quillcrm' ),
+						'label' => __('Coupon Prefix', 'quillcrm'),
 					),
 					'discount_type'      => array(
 						'type'    => 'discount_type_with_amount',
-						'label'   => __( 'Discount Type', 'quillcrm' ),
+						'label'   => __('Discount Type', 'quillcrm'),
 						'options' => array(
-							'fixed_cart'    => __( 'Fixed cart discount', 'quillcrm' ),
-							'fixed_product' => __( 'Fixed product discount', 'quillcrm' ),
-							'percent'       => __( 'Percentage discount', 'quillcrm' ),
+							'fixed_cart'    => __('Fixed cart discount', 'quillcrm'),
+							'fixed_product' => __('Fixed product discount', 'quillcrm'),
+							'percent'       => __('Percentage discount', 'quillcrm'),
 						),
 					),
 					'coupon_expiry_date' => array(
 						'type'  => 'coupon_expiry_date',
-						'label' => __( 'Coupon Expiry Date', 'quillcrm' ),
+						'label' => __('Coupon Expiry Date', 'quillcrm'),
 					),
 					'is_free_shipping'   => array(
 						'type'  => 'checkbox',
-						'label' => __( 'Is Free Shipping', 'quillcrm' ),
+						'label' => __('Is Free Shipping', 'quillcrm'),
 					),
 				),
 			),
 			'limit'   => array(
 				'type'   => 'group',
-				'label'  => __( 'Limit', 'quillcrm' ),
+				'label'  => __('Limit', 'quillcrm'),
 				'fields' => array(
 					'usage_limit_per_coupon' => array(
 						'type'  => 'number',
-						'label' => __( 'Usage Limit Per Coupon', 'quillcrm' ),
+						'label' => __('Usage Limit Per Coupon', 'quillcrm'),
 					),
 					'limit_usage_to_x_items' => array(
 						'type'  => 'number',
-						'label' => __( 'Limit usage to X items', 'quillcrm' ),
+						'label' => __('Limit usage to X items', 'quillcrm'),
 					),
 					'usage_limit_per_user'   => array(
 						'type'  => 'number',
-						'label' => __( 'Usage Limit Per User', 'quillcrm' ),
+						'label' => __('Usage Limit Per User', 'quillcrm'),
 					),
 				),
 			),
@@ -333,7 +328,8 @@ class Create_Coupon extends Action {
 	 *
 	 * @return array
 	 */
-	public function get_attributes_schema() {
+	public function get_attributes_schema()
+	{
 		return array(
 			'type'       => 'object',
 			'properties' => array(

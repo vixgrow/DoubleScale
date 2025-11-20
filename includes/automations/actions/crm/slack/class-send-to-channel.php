b39437be73c45276d4ace0bd04bd22654a5dd75e
ahmedgalal2001 <ahmedgalalmohamed912@gmail.com>
2025-11-20 14:13:00 +0200

    refactor(slack): update Send_To_Channel class to extend Action_Pro and remove legacy methods
    - Changed Send_To_Channel class to extend Action_Pro, indicating it as a Pro feature.
    - Removed the legacy process_action method and related functionality to streamline the codebase.
    - Improved code organization by eliminating unnecessary attributes and methods.

--- includes/automations/actions/crm/slack/class-send-to-channel.php
+++ includes/automations/actions/crm/slack/class-send-to-channel.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class Send_To_Channel
  *
@@ -11,17 +12,12 @@
 
 namespace QuillCRM\Automations\Actions\CRM\Slack;
 
-use QuillCRM\Abstracts\Action;
-use QuillCRM\Managers\Actions_Manager;
-use QuillCRM\Models\Automation_Model;
-use QuillCRM\Models\Automation_Step_Model;
-use QuillCRM\Models\Automation_Contact_Model;
-use QuillCRM\Managers\Integrations_Manager;
+use QuillCRM\Abstracts\Action_Pro;
 
 /**
  * Send To Channel class
  */
-class Send_To_Channel extends Action {
+class Send_To_Channel extends Action_Pro {
 
 	/**
 	 * Action Name
@@ -57,133 +53,6 @@ class Send_To_Channel extends Action {
 	 * @var string
 	 */
 	public $description = 'This action will send a message to a channel in Slack.';
-
-	/**
-	 * Process Action
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param Automation_Model         $automation Automation Model.
-	 * @param Automation_Step_Model    $step Automation Step Model.
-	 * @param Automation_Contact_Model $contact Contact Model.
-	 *
-	 * @return bool
-	 */
-	public function process_action( Automation_Model $automation, Automation_Step_Model $step, Automation_Contact_Model $automation_contact ) {
-		$message = $this->merge_tags_manager->process_merge_tags( $step->get_setting( 'message', '' ), $automation_contact );
-		$channel = $step->get_setting( 'channel', '' );
-		if ( empty( $message ) || empty( $channel ) ) {
-			quillcrm_get_logger()->error(
-				__( 'Slack Send To Channel action is missing required fields.', 'quillcrm' ),
-				array(
-					'code' => 'slack_send_to_channel',
-					'data' => array(
-						'automation' => array(
-							'id'   => $automation->id,
-							'name' => $automation->name,
-						),
-						'step'       => array(
-							'id' => $step->id,
-						),
-					),
-				)
-			);
-			return false;
-		}
-
-		$integration = Integrations_Manager::instance()->get_integration( 'slack' );
-		$api         = $integration->connect();
-
-		if ( ! $api ) {
-			quillcrm_get_logger()->error(
-				__( 'Slack Send To Channel action failed to connect to Slack.', 'quillcrm' ),
-				array(
-					'code' => 'slack_send_to_channel',
-					'data' => array(
-						'automation' => array(
-							'id'   => $automation->id,
-							'name' => $automation->name,
-						),
-						'step'       => array(
-							'id' => $step->id,
-						),
-					),
-				)
-			);
-			return false;
-		}
-
-		$result = $api->post_message( $channel, $message );
-		if ( ! $result['success'] ) {
-			quillcrm_get_logger()->error(
-				__( 'Slack Send To Channel action failed to send message.', 'quillcrm' ),
-				array(
-					'code'     => 'slack_send_to_channel',
-					'data'     => array(
-						'automation' => array(
-							'id'   => $automation->id,
-							'name' => $automation->name,
-						),
-						'step'       => array(
-							'id' => $step->id,
-						),
-					),
-					'response' => $result,
-				)
-			);
-			return false;
-		}
-
-		quillcrm_get_logger()->info(
-			__( 'Slack Send To Channel action sent message.', 'quillcrm' ),
-			array(
-				'code'     => 'slack_send_to_channel',
-				'response' => $result,
-			)
-		);
-
-		return true;
-	}
-
-	/**
-	 * Get attributes schema
-	 *
-	 * @return array
-	 */
-	public function get_attributes_schema() {
-		return array(
-			'type'       => 'object',
-			'properties' => array(
-				'message' => array(
-					'type'     => 'string',
-					'required' => true,
-				),
-				'channel' => array(
-					'type'     => 'string',
-					'required' => true,
-				),
-			),
-		);
-	}
-
-	/**
-	 * Get fields
-	 *
-	 * @return array
-	 */
-	public function get_fields() {
-		return array(
-			'channel' => array(
-				'type'     => 'api_select',
-				'label'    => __( 'Channel', 'quillcrm' ),
-				'endpoint' => 'slack/conversations',
-			),
-			'message' => array(
-				'type'  => 'textarea',
-				'label' => __( 'Message', 'quillcrm' ),
-			),
-		);
-	}
 }
 
 Send_To_Channel::instance();

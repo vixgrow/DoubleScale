a3dff2950a62a2ea973566df3136a719a0a9ef10
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2025-11-24 09:21:15 +0200

    feat(twilio-integration): add Twilio integration stubs for free version
    - Introduced stub classes for Twilio integration, including Integration, Remote_Data, and REST_Controller, to provide a placeholder for Pro features.
    - Implemented a "Pro Feature" notice in the integration card to inform users about the upgrade requirement for full functionality.
    - Enhanced the Integrations_Manager to support Pro feature checks and registration of integrations.

---
+++ includes/automations/integrations/twilio/class-integration.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Class Twilio (Free Version Stub)
+ *
+ * This is a stub integration that shows "Pro Feature" notice in free plugin
+ * The Pro plugin will override this with the actual Twilio integration
+ *
+ * @since 1.0.0
+ *
+ * @package QuillCRM
+ */
+
+namespace QuillCRM\Automations\Integrations\Twilio;
+
+use QuillCRM\Abstracts\Integration as Integration_Abstract;
+use QuillCRM\Managers\Integrations_Manager;
+
+/**
+ * Twilio stub class for free plugin
+ */
+class Integration extends Integration_Abstract {
+
+	/**
+	 * Integration Name
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.0
+	 */
+	public $name = 'Twilio';
+
+	/**
+	 * Integration Slug
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.0
+	 */
+	public $slug = 'twilio';
+
+	/**
+	 * Integration Description
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.0
+	 */
+	public $description = 'Send SMS and WhatsApp messages to your contacts. Twilio integration is a Pro feature.';
+
+	/**
+	 * Is Pro feature
+	 *
+	 * @var bool
+	 *
+	 * @since 1.0.0
+	 */
+	public $is_pro = true;
+
+	/**
+	 * Option name
+	 *
+	 * @var string
+	 *
+	 * @since 1.0.0
+	 */
+	public $option_name = 'twilio';
+
+	/**
+	 * Class names
+	 *
+	 * @var array
+	 */
+	protected static $classes = array(
+		'remote_data'     => Remote_Data::class,
+		'rest_controller' => REST_Controller::class,
+	);
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		// Call parent constructor to initialize REST controller and remote data
+		parent::__construct();
+	}
+
+	/**
+	 * Connect the integration (stub - always returns false)
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return bool
+	 */
+	public function connect() {
+		return false;
+	}
+
+	/**
+	 * Validate (stub - always returns error)
+	 *
+	 * @param array $settings Settings.
+	 *
+	 * @return \WP_Error
+	 */
+	public function validate( $settings ) {
+		return new \WP_Error( 'pro_feature', __( 'Twilio integration is a Pro feature. Please upgrade to QuillCRM Pro.', 'quillcrm' ) );
+	}
+
+	/**
+	 * Get fields (returns empty array in free version)
+	 *
+	 * @return array
+	 */
+	public function get_fields() {
+		return array();
+	}
+}
+
+// Register the stub integration in free plugin
+// Pro plugin will override this registration
+Integrations_Manager::instance()->register( new Integration() );
+

e82217948fca420b8de6f4f6430572931a021f6d
emy185 <emyy729@gmail.com>
2025-12-01 15:56:06 +0200

    feat(pro-automation-modal): add pro feature image and enhance modal UI

--- src/components/pro-feature-notice/index.tsx
+++ src/components/pro-feature-notice/index.tsx
@@ -13,6 +13,9 @@ import { AlertCircle, Crown, ExternalLink } from 'lucide-react';
  */
 import './style.scss';
 import config from '../../config';
+//@ts-ignore
+import proImage from '../../../assets/images/pro_img.png';
+import { RocketIcon } from '../icons';
 
 interface ProFeatureNoticeProps {
 	featureName: string;
@@ -30,15 +33,9 @@ export const ProFeatureNotice: React.FC<ProFeatureNoticeProps> = ({
 	return (
 		<div className="qcrm-pro-feature-notice">
 			<div className="qcrm-pro-feature-notice__container">
-				<div className="qcrm-pro-feature-notice__icon">
-					<Crown size={48} />
-				</div>
+				<img src={proImage} alt="Pro Feature" />
 				<div className="qcrm-pro-feature-notice__content">
 					<h2 className="qcrm-pro-feature-notice__title">
-						<Crown
-							size={24}
-							className="qcrm-pro-feature-notice__title-icon"
-						/>
 						{featureName} {__('is a Pro Feature', 'quillcrm')}
 					</h2>
 					{description && (
@@ -46,39 +43,38 @@ export const ProFeatureNotice: React.FC<ProFeatureNoticeProps> = ({
 							{description}
 						</p>
 					)}
-					{features.length > 0 && (
+					{/* {features.length > 0 && (
 						<div className="qcrm-pro-feature-notice__features">
 							<h3>{__('Pro Features Include:', 'quillcrm')}</h3>
 							<ul>
 								{features.map((feature) => (
 									<li key={feature}>
-										<AlertCircle size={16} />
+										<AlertCircle size={16} className='text-[#458DC7]'/>
 										{feature}
 									</li>
 								))}
 							</ul>
 						</div>
-					)}
-					<div className="qcrm-pro-feature-notice__actions">
-						<a
-							href={upgradeUrl}
-							className="qcrm-pro-feature-notice__button qcrm-pro-feature-notice__button--primary"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<Crown size={20} />
-							{__('Upgrade to Pro', 'quillcrm')}
-							<ExternalLink size={16} />
-						</a>
+					)} */}
+					<div className="qcrm-pro-feature-notice__actions mt-5">
 						<a
 							href={`${upgradeUrl}#features`}
 							className="qcrm-pro-feature-notice__button qcrm-pro-feature-notice__button--secondary"
 							target="_blank"
 							rel="noopener noreferrer"
 						>
-							{__('Learn More', 'quillcrm')}
-							<ExternalLink size={16} />
+							{__('Try a Free demo', 'quillcrm')}
 						</a>
+						<a
+							href={upgradeUrl}
+							className="qcrm-pro-feature-notice__button qcrm-pro-feature-notice__button--primary"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<RocketIcon />
+							{__('Upgrade to Pro', 'quillcrm')}
+						</a>
+						
 					</div>
 				</div>
 			</div>

2a17c4ef82219e5467b88d5a68f1ed1bca51605d
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2026-05-13 11:17:10 +0300

    Refactor build script and update Card component styles for consistency

--- src/client/pages/booking/calendars/index.tsx
+++ src/client/pages/booking/calendars/index.tsx
@@ -596,7 +596,7 @@ const Calendars: React.FC = () => {
 												className="bg-[#FDFDFD] mb-4"
 											><CardContent>
 													<div className='flex flex-col gap-5'>
-														<Card className="bg-white"><CardContent>
+														<Card className="bg-white"><CardContent className="py-5">
 															<div className='flex justify-between items-center'>
 																<div className='flex flex-col'>
 																	<div className="text-[#313131] text-base font-semibold">
